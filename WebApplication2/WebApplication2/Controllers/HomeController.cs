fceb08d1d7af6f8a19977964982746a2f8d107ee
EshginKazimov <82080797+EshginKazimov@users.noreply.github.com>
2021-05-04 23:04:53 +0400

    Add files via upload

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController
+    {
+        public string Index(int id)
+        {
+            return "Hello world";
+        }
+    }
+}
